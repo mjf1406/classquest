0a29570869e9cc312a2a293318c0637580cc5bf3
Michael J. Fitzgerald <michael.fitzgerald.1406@gmail.com>
2025-06-20 16:22:08 +0900

    fix 1mb limit?

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
 /** @type {import("next").NextConfig} */
 const config = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "10mb", // Increase from default 1MB to 10MB
+    },
+  },
   redirects: async () => {
     return [
       {
         source: "/",
         destination: "/classes",
         permanent: true, // or false for temporary redirect
-        experimental: {
-          serverActions: {
-            bodySizeLimit: "10mb", // Increase from default 1MB to 10MB
-          },
-        },
       },
     ];
   },
